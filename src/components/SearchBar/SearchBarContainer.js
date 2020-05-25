953bb4a59b3ee97822601b51299f7f5b227c9804
Donna Ballew <ballewdd@gmail.com>
2020-05-24 21:58:07 -0700

    added styling to get clone to resemble the clone - Stretch

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,21 +1,23 @@
 // You do not need to change any code in this file
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from "@fortawesome/free-regular-svg-icons";
 import "./SearchBar.css";
 
 const SearchBar = () => {
   return (
     <div className="search-bar-wrapper">
-      <div className="social">
+      <div className="social-wrapper">
         <FontAwesomeIcon icon={faInstagram} />
+        <h1 className="instagram-logo">Instagram</h1>
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <input type="text" placeholder="Search" />
       </form>
       <div className="social-wrapper">
         <div className="social">
